88281ac6ed4e0da35e438bd3c430092f7802729c
Andres Sanchez <andres.gedeon@gmail.com>
2017-11-25 09:30:58 -0500

    Refactorizo using y elimino linea de migracion

--- GestionNovedades.Model/Entidades/GIOContext.cs
+++ GestionNovedades.Model/Entidades/GIOContext.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace GestionNovedades.Model.Entidades
 {
-    class GIOContext:DbContext
+    using System.Data.Entity;
+
+    public class GIOContext:DbContext
     {
         public DbSet<Company> Companies { get; set; }
         public DbSet<Settled> Problems { set; get; }
@@ -15,7 +12,7 @@ namespace GestionNovedades.Model.Entidades
 
         public GIOContext(): base("GIOContext") 
         {
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<GIOContext, Configuration>("GIOContext"));
+            
         }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
