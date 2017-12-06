f648f50f5574926bc49fd8a54d168b4852d89d4c
Andres Sanchez <andres.gedeon@gmail.com>
2017-12-05 20:20:48 -0500

    Incluimos Test an DAO company

---
+++ GestionNovedades.Model/ResourceAccess/DAO/CompanyDAO.cs
@@ -0,0 +1,108 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CompanyDAO.cs" company="OPA SAS">
+//   All Rights Reserved
+// </copyright>
+// <summary>
+//   Defines the CompanyDAO type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GestionNovedades.Model.ResourceAccess.DAO
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using GestionNovedades.Model.Entidades;
+
+    /// <summary>
+    /// The company dao.
+    /// </summary>
+    public class CompanyDao
+    {
+        /// <summary>
+        /// The insert.
+        /// </summary>
+        /// <param name="company">
+        /// The company.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Guid"/>.
+        /// </returns>
+        internal Guid Insert(Company company)
+        {
+            using (GioContext context = new GioContext())
+            {
+                context.Companies.Add(company);
+                context.SaveChanges();
+                return company.CompanyId;
+            }
+
+        }
+
+        /// <summary>
+        /// The update.
+        /// </summary>
+        /// <param name="company">
+        /// The company.
+        /// </param>
+        internal void Update(Company company)
+        {
+            using (GioContext context = new GioContext())
+            {
+                var companiaBd = context.Companies.FirstOrDefault(p => p.CompanyId == company.CompanyId);
+                if (companiaBd != null)
+                {
+                    companiaBd.CompanyName = company.CompanyName;
+                    context.SaveChanges();
+                }
+            }                            
+        }
+
+        /// <summary>
+        /// The delete.
+        /// </summary>
+        /// <param name="company">
+        /// The company.
+        /// </param>
+        internal void Delete(Company company)
+        {
+            using (GioContext context = new GioContext())
+            {
+                context.Companies.Remove(company);
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// The get company.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="List"/>.
+        /// </returns>
+        internal List<Company> GetCompany()
+        {
+            using (GioContext context = new GioContext())
+            {
+                return context.Companies.ToList();
+            }
+        }
+
+        /// <summary>
+        /// The get company.
+        /// </summary>
+        /// <param name="companyId">
+        /// The company id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Company"/>.
+        /// </returns>
+        internal Company GetCompany(Guid companyId)
+        {
+            using (GioContext context = new GioContext())
+            {
+                return context.Companies.FirstOrDefault(p => p.CompanyId == companyId);
+            }
+        }
+    }
+}
---
+++ GestionNovedades.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GestionNovedades.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GestionNovedades.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3a6d3467-cc9c-4bf4-b2cc-61d89e2a9ce3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
